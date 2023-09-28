5db5d7e9e3808704fb82c3a3e29fed2996bcef44
avoytkiv <74664634+avoytkiv@users.noreply.github.com>
2023-09-28 04:51:29 +0000

    Corrected cloud and src scripts for split stage

--- src/common.py
+++ src/common.py
@@ -1,12 +1,15 @@
 """Common code and variables."""
 
+# Data
 TARGET_COLUMN = "Log1pSalary"
 CATEGORICAL_COLUMNS = ["Category", "ContractType", "ContractTime"]
+# Directories
 DATA_DIR = "data/"
+PROCESSED_DATA_DIR = "data/processed/"
 MODEL_DIR = "model/"
 PLOTS_DIR = "plots/"
+# Parameters
 RANDOM_STATE = 42
-
 MAX_LEN = 10
 WORD_DROPOUT_RATE = 0
 BATCH_SIZE = 512
